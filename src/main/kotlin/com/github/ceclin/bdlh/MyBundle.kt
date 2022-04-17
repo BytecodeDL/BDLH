cd6c2b23c8892fe97f03260304beaf5adea42944
GitHub Action <action@github.com>
2022-04-17 03:39:18 +0000

    Template cleanup

--- src/main/kotlin/com/github/ceclin/bdlh/MyBundle.kt
+++ src/main/kotlin/com/github/ceclin/bdlh/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ceclin.bdlh
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ceclin/bdlh/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ceclin/bdlh/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ceclin.bdlh.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ceclin.bdlh.MyBundle
 
 class MyApplicationService {
 
